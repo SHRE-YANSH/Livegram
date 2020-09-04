40c34fa4ba4a36b03a7dcc05ac439e9459f08389
okay-retard <68938378+okay-retard@users.noreply.github.com>
2020-09-04 12:13:04 +0530

    Create __init__.py

---
+++ Livegram/mod/sql/__init__.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from .. import DB_URI
+
+
+def start() -> scoped_session:
+    """ returns SQLAlchemy ScopedSession """
+    engine = create_engine(DB_URI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(
+        sessionmaker(
+            bind=engine,
+            autoflush=False
+        )
+    )
+
+
+BASE = declarative_base()
+SESSION = start()
